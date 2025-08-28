de5b173873eddb35c0444f29c69ba0cf400b11cc
Santosh Kumar <160557558+santoshdiary@users.noreply.github.com>
2025-08-28 08:20:12 +0530

    indtroduction to js

---
+++ javaScript/operation.js
@@ -0,0 +1,53 @@
+
+let someNumber = 123;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // exponentiation
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+/*  If any operand is a string, + concatenates as a string.
+Evaluation is left to right.
+If both operands are numbers, it adds numerically.
+As soon as a string is involved, everything after becomes string concatenation.
+*/ 
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
---
+++ javaScript/variable.js
@@ -0,0 +1,15 @@
+const accountId=23232;
+let name="Santosh Kushwaha";
+var password=2232;
+// accountId=2; // not allowed 
+
+city="Ranchi"
+console.log(city);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+email="santosh.skm2022@gmail.com"
+console.table([city,accountId,password, email])
