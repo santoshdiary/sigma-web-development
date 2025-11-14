f629ea0d4d68637cbdc54846de7d447af0cd2961
Santosh Kumar <160557558+santoshdiary@users.noreply.github.com>
2025-11-14 17:21:08 +0530

    overview of stack and heap

--- javaScript/10stack&heap.js
+++ javaScript/10stack&heap.js
@@ -1,14 +1,15 @@
 /* 
- As usual in javascript there are two memory locations
+ As usual in javascript there are two memory locations that used in programing
  1. Stack       
  2. Heap
+
 All the primitive data type varibale is stored in stack 
 and non primitive data type varibale is stored in Heap 
 
 stack value is not given direct access only copy value is given 
 to other variable and changes not reflect on it
 
-while Heap variable gives reference to the other variable and 
+while Heap variable gives reference(orignal valuse) to the other variable and 
 changes reflect on original variable */
 
 let num1 = 6; // stored in stack
