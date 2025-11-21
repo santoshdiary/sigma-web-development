b75f0b1aa8178dce9d59453af0b5ccd5de5dc7f3
Santosh Kumar <160557558+santoshdiary@users.noreply.github.com>
2025-11-21 11:29:55 +0530

    last session of object - json

---
+++ javaScript/02basics/5objectDeStructure.js
@@ -0,0 +1,31 @@
+//Object destructuring in JavaScript is a syntax that allows you to extract specific properties from an object and assign them to variables in a clean, short, readable way.
+//It is a shorthand way to unpack values from objects into separate variables.
+
+const course={
+    name:"javaScript",
+    duration:3,
+    instructor:"Hitesh Choudhary "
+
+}
+// console.log(course.insturctor); // this syntax is fine, but repetition of "course.something" is  perfomed to access the data 
+const { instructor:crsIns} = course
+// console.log(instructor);
+console.log(crsIns);
+
+
+//JSON - javascript object notation
+//It is a data format used to send and receive data between server and client.
+// {
+//   "name": "Rahul",
+//   "age": 22,
+//   "city": "Delhi"
+// }
+//API can return vlaue in object like above or array like below-
+[
+    {},
+    {},
+    {}
+]
+
+
+
