70b6cfcedde87616df1467aaef1a4ac45b84e39c
Santosh Kushwaha <santosh.skm2019@gmail.com>
2026-06-14 12:00:24 +0530

    react project created

---
+++ javaScript/ReachJsTutorial/React-Learnings/src/App.jsx
@@ -0,0 +1,8 @@
+function App(){ // it's just a funtion in which what we do, reflected on screen
+  return <h1>Hello World with React</h1>;
+}
+export default App;
+
+
+
+// sortcut "race" - react arro function component export
