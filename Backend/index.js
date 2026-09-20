5afd5a51f33ecb8186d40428a2303077e6b7428c
Santosh Kushwaha <santosh.skm2019@gmail.com>
2026-09-20 13:49:51 +0530

    introduction of srver creation

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express(); // storing exprss in a variable "app"
+
+app.get("/", (req, res) => {   // sending get request to server that in / (home directory), callback include to parameters request and response & eventally server respose hello world 
+  res.send("Hello World");
+});
+
+app.get("/profile", (req, res) => {
+  res.send("this is profile");
+});
+app.get("/loginpage", (req, resp) => {
+  resp.send("<h1>THIS IS LOGIN PAGE </h1>");
+});
+
+app.listen(3000, () => { // once server starts, it continuously listen in given port  
+  console.log("Server is running on http://localhost:3000");
+});
